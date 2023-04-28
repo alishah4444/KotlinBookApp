015ab08e4b9c0ae1cfc994a5920295781bdc5202
zhanhb <6323014+zhanhb@users.noreply.github.com>
2023-04-29 04:41:52 +0800

    Initial commit

---
+++ src/main/java/com/example/OpenAiConfiguration.java
@@ -0,0 +1,21 @@
+package com.example;
+
+import com.theokanning.openai.service.OpenAiService;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenAiConfiguration {
+
+    private final String token;
+
+    public OpenAiConfiguration(@Value("${openai.token}") String token) {
+        this.token = token;
+    }
+
+    @Bean
+    public OpenAiService openAiService() {
+        return new OpenAiService(token);
+    }
+}
---
+++ src/main/java/com/example/OpenAiHttpExceptionHandler.java
@@ -0,0 +1,23 @@
+package com.example;
+
+import com.theokanning.openai.OpenAiError;
+import com.theokanning.openai.OpenAiHttpException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class OpenAiHttpExceptionHandler {
+
+    @ExceptionHandler
+    public ResponseEntity<OpenAiError> handle(OpenAiHttpException ex) {
+        OpenAiError oae = new OpenAiError();
+        oae.error = new OpenAiError.OpenAiErrorDetails();
+        oae.error.setMessage(ex.getMessage());
+        oae.error.setType(ex.type);
+        oae.error.setParam(ex.param);
+        oae.error.setCode(ex.code);
+        return ResponseEntity.status(ex.statusCode).body(oae);
+    }
+
+}
