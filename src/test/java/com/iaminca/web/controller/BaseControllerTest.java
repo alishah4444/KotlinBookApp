9102849b0ea33cbf2a12118df87cc5b7d93d7ae6
zhanhb <6323014+zhanhb@users.noreply.github.com>
2023-05-05 05:02:08 +0800

    add more tests

--- src/test/java/com/iaminca/web/controller/BaseControllerTest.java
+++ src/test/java/com/iaminca/web/controller/BaseControllerTest.java
@@ -1,7 +1,14 @@
 package com.iaminca.web.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.theokanning.openai.completion.CompletionChunk;
+import com.theokanning.openai.completion.CompletionRequest;
+import com.theokanning.openai.completion.CompletionResult;
+import com.theokanning.openai.completion.chat.ChatCompletionChunk;
 import com.theokanning.openai.completion.chat.ChatCompletionRequest;
 import com.theokanning.openai.completion.chat.ChatMessage;
+import lombok.Lombok;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,12 +21,28 @@ import org.springframework.test.web.reactive.server.WebTestClient;
 
 import java.time.Duration;
 import java.util.Collections;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.function.Function;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class BaseControllerTest {
 
     @Autowired
     private WebTestClient webClient;
+    @Autowired
+    private ObjectMapper mapper;
+
+    private <T> Function<String, T> uncheckedReadValue(Class<T> clazz) {
+        return data -> {
+            try {
+                return mapper.readValue(data, clazz);
+            } catch (JsonProcessingException ex) {
+                throw Lombok.sneakyThrow(ex);
+            }
+        };
+    }
 
     @Test
     public void testListModels() {
@@ -30,22 +53,82 @@ public class BaseControllerTest {
                 .jsonPath("$.data").isArray();
     }
 
+    private CompletionRequest.CompletionRequestBuilder completionRequestBuilder() {
+        return CompletionRequest.builder()
+                .model("ada")
+                .prompt("Somebody once told me the world is gonna roll me")
+                .echo(true)
+                .user("testing")
+                .n(3);
+    }
+
     @Test
-    public void testCreateChatCompletion() {
-        ChatCompletionRequest request = ChatCompletionRequest.builder()
+    public void testCreateCompletion() {
+        CompletionRequest request = completionRequestBuilder().build();
+        assertThat(webClient.post().uri("/v1/completions").bodyValue(request).exchange()
+                .expectStatus().isOk()
+                .expectHeader().contentTypeCompatibleWith(MediaType.APPLICATION_JSON)
+                .returnResult(CompletionResult.class)
+                .getResponseBody().single().blockOptional().map(CompletionResult::getId)).isNotEmpty();
+    }
+
+    private <Tp, Id> void assertSseIdentity(
+            WebTestClient.RequestHeadersSpec<?> spec, Class<Tp> chunkClass, Function<Tp, Id> identity) {
+        spec.exchange()
+                .expectStatus().isOk()
+                .expectHeader().contentTypeCompatibleWith(MediaType.TEXT_EVENT_STREAM)
+                .returnResult(String.class)
+                .consumeWith(it -> {
+                    AtomicBoolean bool = new AtomicBoolean();
+                    assertThat(it.getResponseBody()
+                            .takeWhile(x -> {
+                                if ("[DONE]".equals(x)) {
+                                    bool.set(true);
+                                    return false;
+                                }
+                                return true;
+                            })
+                            .map(uncheckedReadValue(chunkClass))
+                            .map(identity).distinct().toStream()).hasSize(1);
+                    assertThat(bool).isTrue();
+                });
+    }
+
+    @Test
+    public void testStreamCompletion() {
+        CompletionRequest request = completionRequestBuilder().stream(true).build();
+        assertSseIdentity(
+                webClient.post().uri("/v1/completions").bodyValue(request),
+                CompletionChunk.class, CompletionChunk::getId);
+    }
+
+    private ChatCompletionRequest.ChatCompletionRequestBuilder chatCompletionRequestBuilder() {
+        return ChatCompletionRequest.builder()
                 .model("gpt-3.5-turbo")
                 .messages(Collections.singletonList(
                         new ChatMessage("system", "You are a dog and will speak as such.")
                 ))
                 .n(1)
                 .maxTokens(50)
-                .logitBias(Collections.emptyMap())
-                .build();
+                .logitBias(Collections.emptyMap());
+    }
+
+    @Test
+    public void testCreateChatCompletion() {
+        ChatCompletionRequest request = chatCompletionRequestBuilder().build();
         webClient.post().uri("/v1/chat/completions").bodyValue(request).exchange()
                 .expectStatus().isOk()
                 .expectHeader().contentType(MediaType.APPLICATION_JSON)
                 .expectBody()
                 .jsonPath("$.usage.total_tokens").value(Matchers.greaterThan(0));
+    }
+
+    @Test
+    public void testStreamChatCompletion() {
+        ChatCompletionRequest request = chatCompletionRequestBuilder().stream(true).build();
+        assertSseIdentity(
+                webClient.post().uri("/v1/chat/completions").bodyValue(request),
+                ChatCompletionChunk.class, ChatCompletionChunk::getId);
     }
 
     @TestConfiguration
