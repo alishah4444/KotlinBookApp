8b5faa1780de98b0650cdc2163010675ae7c4fb7
zhanhb <6323014+zhanhb@users.noreply.github.com>
2023-05-04 13:42:07 +0800

    Add ExceptionHandler for HttpException

--- src/main/java/com/iaminca/OpenAiHttpExceptionHandler.java
+++ src/main/java/com/iaminca/OpenAiHttpExceptionHandler.java
@@ -2,9 +2,14 @@ package com.iaminca;
 
 import com.theokanning.openai.OpenAiError;
 import com.theokanning.openai.OpenAiHttpException;
+import okhttp3.ResponseBody;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import retrofit2.HttpException;
+import retrofit2.Response;
+
+import java.io.IOException;
 
 @ControllerAdvice
 public class OpenAiHttpExceptionHandler {
@@ -18,6 +23,21 @@ public class OpenAiHttpExceptionHandler {
         oae.error.setParam(ex.param);
         oae.error.setCode(ex.code);
         return ResponseEntity.status(ex.statusCode).body(oae);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<?> handle(HttpException ex) throws IOException {
+        ResponseEntity.BodyBuilder builder = ResponseEntity.status(ex.code());
+        Response<?> response = ex.response();
+        if (response != null) {
+            builder.headers(headers -> headers.putAll(response.headers().toMultimap()));
+            try (ResponseBody responseBody = response.errorBody()) {
+                if (responseBody != null) {
+                    return builder.body(responseBody.bytes());
+                }
+            }
+        }
+        return builder.build();
     }
 
 }
