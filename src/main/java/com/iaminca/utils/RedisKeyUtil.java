b923d3b30b4d050c44f7731fa96d1ba2b93b591e
juzi <1277345601@qq.com>
2023-05-15 10:15:44 -0400

    change redis key

--- src/main/java/com/iaminca/utils/RedisKeyUtil.java
+++ src/main/java/com/iaminca/utils/RedisKeyUtil.java
@@ -5,7 +5,7 @@ public class RedisKeyUtil {
 
     public static String USER_REDIS_REGISTER_DIRE = "user:register:";
     public static String USER_REDIS_INFO_DIRE = "user:info:";
-    public static String USER_REDIS_PHONE_TOKEN_DIRE = "user:info:phoneGetToken";
+    public static String USER_REDIS_PHONE_TOKEN_DIRE = "user:info:phone:token:";
 
     public static String GPT_REDIS_USERINFO_DIRE = "gpt:user:info:";
     public static String GPT_REDIS_USER_BALANCE_DIRE = "gpt:user:balance:";
