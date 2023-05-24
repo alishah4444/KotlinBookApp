3682024cff3addac187525bbce9d58a5d8098071
zhanhb <6323014+zhanhb@users.noreply.github.com>
2023-05-24 22:54:31 +0800

    配置上传文件路径

--- src/main/java/com/iaminca/web/controller/UserFileController.java
+++ src/main/java/com/iaminca/web/controller/UserFileController.java
@@ -4,11 +4,9 @@ import com.iaminca.common.ErrorCode;
 import com.iaminca.exception.BusinessException;
 import com.iaminca.utils.DateUtil;
 import com.iaminca.web.controller.base.UserBaseController;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.FilenameUtils;
-import org.apache.commons.lang3.time.DateUtils;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.codec.multipart.FilePart;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +14,8 @@ import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Arrays;
@@ -23,28 +23,33 @@ import java.util.Date;
 import java.util.List;
 
 @RestController
-@RequiredArgsConstructor
 @RequestMapping("/userFile")
 @Slf4j
 public class UserFileController extends UserBaseController {
 
-    private List<String> imageList = Arrays.asList("jpg","jpeg","png");
-    private String FILE_PATH="/tmp/";
+    private static final List<String> imageList = Arrays.asList("jpg", "jpeg", "png");
+    private final Path imageSavePath;
 
+    public UserFileController(@Value("${upload.recharge.path}") String rechargeImageDir) throws IOException {
+        imageSavePath = Paths.get(rechargeImageDir);
+        if (!Files.isDirectory(imageSavePath)) {
+            Files.createDirectories(imageSavePath);
+        }
+        log.info("imageSavePath: {}", rechargeImageDir);
+    }
 
     @PostMapping("/v1/saveImage")
     public Mono<Object> uploadFile(@RequestPart("file") FilePart file) {
         String extension = FilenameUtils.getExtension(file.filename());
-        if(!imageList.contains(extension.toLowerCase())){
+        if (!imageList.contains(extension.toLowerCase())) {
             throw new BusinessException(ErrorCode.USER_FILE_SUFFIX_ERROR);
         }
         String format = DateUtil.format(new Date(), DateUtil.SHORT_DATE_PATTERN_2);
-        String filePath = FILE_PATH;
-        Path target = Paths.get(filePath, System.currentTimeMillis()+"."+extension);
+        Path target = imageSavePath.resolve(format + System.nanoTime() + "." + extension);
 
         return file.transferTo(target).then(Mono.fromCallable(() -> {
             // save to db
-            return filePath+target.getFileName().toString();
+            return target.getFileName().toString();
         }));
     }
 
